aa612205a6003809bd085a1c9dce867b6aaa7e12
Mohsen Naderinejad <mhsn.naderinejad@gmail.com>
2026-05-19 00:42:09 +0330

    Register Document and DocumentChunk models in the admin interface with inline editing support

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Document, DocumentChunk
 
-# Register your models here.
+
+class DocumentChunkInline(admin.TabularInline):
+    model = DocumentChunk
+    extra = 0
+    readonly_fields = ['chunk_index', 'content']
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'uploaded_at', 'updated_at']
+    search_fields = ['title', 'extracted_text']
+    readonly_fields = ['extracted_text', 'uploaded_at', 'updated_at']
+    inlines = [DocumentChunkInline]
