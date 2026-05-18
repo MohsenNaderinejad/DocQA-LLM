ff64060808f1040972e4a9a727c323bbb8ea09cc
Mohsen Naderinejad <mhsn.naderinejad@gmail.com>
2026-05-19 00:42:14 +0330

    Register QAHistory model in the admin interface with custom display and readonly fields

--- qa/admin.py
+++ qa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import QAHistory
 
-# Register your models here.
+
+@admin.register(QAHistory)
+class QAHistoryAdmin(admin.ModelAdmin):
+    list_display = ['short_question', 'created_at']
+    readonly_fields = ['question', 'answer', 'source_documents', 'created_at']
+
+    def short_question(self, obj):
+        return obj.question[:80]
+    short_question.short_description = 'Question'
