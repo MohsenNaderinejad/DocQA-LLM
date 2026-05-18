9f299765511dbc7199e1f70380dd43d264cf37f9
Mohsen Naderinejad <mhsn.naderinejad@gmail.com>
2026-05-19 01:31:16 +0330

    Refactor process_document and _do_processing functions for improved clarity and comments

--- documents/signals.py
+++ documents/signals.py
@@ -4,27 +4,35 @@ from .models import Document, DocumentChunk
 from .processing import extract_text_from_docx, split_text_into_chunks
 
 
-@receiver(post_save, sender=Document)
+@receiver(post_save, sender=Document) # Singal for checking if a file was saved or chnaged the contents
 def process_document(sender, instance, created, **kwargs):
     if not instance.file:
         return
-
-    if created:
+    
+    if created: # Checking creation of DB new record of file
         _do_processing(instance)
+        return
+    
+    try: # Checking if the file was changed to reprocess it or even if it was deleted
+        old_instance = Document.objects.get(pk=instance.pk)
+        if old_instance.file != instance.file:
+            _do_processing(instance)
+    except Document.DoesNotExist:
+        pass
 
 
 def _do_processing(instance):
     try:
         file_path = instance.file.path
-        extracted_text = extract_text_from_docx(file_path)
+        extracted_text = extract_text_from_docx(file_path) # Extracting Text from docs file
 
         Document.objects.filter(pk=instance.pk).update(
             extracted_text=extracted_text
-        )
+        ) # getting the created document in DB records
         
-        DocumentChunk.objects.filter(document=instance).delete()
+        DocumentChunk.objects.filter(document=instance).delete() # Deleteing all the chunks related to the saved docs
         
-        chunks = split_text_into_chunks(extracted_text)
+        chunks = split_text_into_chunks(extracted_text) # spliting it in 500 chars parts with 50 char overlaps
         chunk_objects = [
             DocumentChunk(
                 document=instance,
@@ -34,7 +42,7 @@ def _do_processing(instance):
             for i, chunk in enumerate(chunks)
         ]
         
-        DocumentChunk.objects.bulk_create(chunk_objects)
+        DocumentChunk.objects.bulk_create(chunk_objects) # Efficent DB query for saving the docs chunks
         
         print(f"$$$ Processed '{instance.title}': {len(chunks)} chunks created")
         
