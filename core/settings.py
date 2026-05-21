0ffcd1e8935db725b41eb0cc9e1a7424626347f5
Mohsen Naderinejad <mhsn.naderinejad@gmail.com>
2026-05-21 17:35:47 +0330

    Add support for PostgreSQL database configuration using DATABASE_URL environment variable

--- core/settings.py
+++ core/settings.py
@@ -79,12 +79,30 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    m = re.match(
+        r'postgresql://(?P<user>[^:]+):(?P<password>[^@]+)@(?P<host>[^:]+):(?P<port>\d+)/(?P<name>.+)',
+        DATABASE_URL
+    )
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': m.group('name'),
+            'USER': m.group('user'),
+            'PASSWORD': m.group('password'),
+            'HOST': m.group('host'),
+            'PORT': m.group('port'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
